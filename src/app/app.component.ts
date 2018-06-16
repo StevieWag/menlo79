08529597edf22a69ac3498a01e869139e818f723
Stevie <s.wagner.hef@hotmail.de>
2018-06-16 11:53:28 +0200

    Prepare for heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './css/bootstrap.css',
+    './fonts/font-awesome/css/font-awesome.css',
+    './css/style.css',
+    'css/nivo-lightbox/nivo-lightbox.css',
+    'css/nivo-lightbox/default.css'
+  ]
 })
 export class AppComponent {
-  title = 'app';
 }
